d84579e9c8f6afee1c9ad0f6b5f69a948e6b83f8
yuzujr <15568103056@163.com>
2026-03-01 20:21:48 +0800

    update

--- home/dot_config/niri/scripts/executable_swayidle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# 10分钟锁屏，30分钟熄屏
-exec swayidle \
-  timeout 600 'qs -c noctalia-shell ipc call lockScreen lock' \
-  timeout 1800 'niri msg action power-off-monitors' \
-  resume 'niri msg action power-on-monitors'
